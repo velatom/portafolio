6833f75afae5978cd90a3dc73658d5a8ca8d3979
velatom <eugeniorc@gmail.com>
2018-06-14 16:51:24 +0200

    Servicio Firebase listo sin html

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -8,14 +8,36 @@ export class InformacionService {
 
 	info:any = {};
 	cargada:boolean = false;
+  cargada_firebase:boolean = false;
+  equipo:any[] = [];
 
   constructor( public http:Http) {
 
-  	this.http.get("assets/data/info.pagina.json")
-  				.subscribe( data =>{
-  					console.log(data.json());
-  					this.cargada = true;
-  					this.info = data.json();
-  				})
+    this.carga_info();
+    this.carga_sobre_firebase();
+  	
   }
+
+  public carga_info(){
+
+    this.http.get("assets/data/info.pagina.json")
+          .subscribe( data =>{
+            //console.log(data.json());
+            this.cargada = true;
+            this.info = data.json();
+          })
+
+  }
+
+  public carga_sobre_firebase(){
+
+    this.http.get("https://paginaweb-10bdb.firebaseio.com/equipo.json")
+          .subscribe( data =>{
+            console.log(data.json());
+            this.cargada_firebase = true;
+            this.equipo = data.json();
+          })
+
+  }
+
 }
