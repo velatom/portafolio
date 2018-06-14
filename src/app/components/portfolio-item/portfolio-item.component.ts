aa64a2e5564c7bf477b21d7b5b60e66dcb249b8a
velatom <eugeniorc@gmail.com>
2018-06-14 10:39:42 +0200

    Portfolio Item creado

---
+++ src/app/components/portfolio-item/portfolio-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-item',
+  templateUrl: './portfolio-item.component.html',
+  styles: []
+})
+export class PortfolioItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
